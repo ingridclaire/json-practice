a0f145e12507b9f933860c04896e95d054025e2c
Ingrid Carlson <ingridclaire@gmail.com>
2023-06-20 12:52:15 -0400

    json practice files

---
+++ script.js
@@ -0,0 +1,6 @@
+// Your task is to request user data from an API, and then visually display that data on screen. There will be 10 users included in the data sent from the API. Be sure to represent all 10 users on screen.
+// API url: https://randomuser.me/api/?results=10&nat=us
+
+
+
+// Next, fetch data directly from the data.json file, and render the content to the DOM
